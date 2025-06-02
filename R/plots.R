48f05018cad83860441302831b4ae15c696255ca
Rob J Hyndman <robjhyndman@gmail.com>
2025-06-02 13:58:58 +1000

    Improved plotting

--- R/plots.R
+++ R/plots.R
@@ -1,19 +1,42 @@
-make_plot1 <- function(downloads) {
-  downloads |>
-    filter(Package == "forecast") |>
-    ggplot(aes(x = Month, y = Count / 1e3)) +
-    geom_line() +
-    labs(y = "Downloads (thousands)")
-}
-make_plot2 <- function(
+make_download_plot <- function(
   downloads,
-  top_ten,
+  packages,
   first_month = yearmonth("2000-Jan")
 ) {
-  downloads |>
-    filter(Package %in% top_ten$Package) |>
-    filter(Package != "forecast", Month >= first_month) |>
-    ggplot(aes(x = Month, y = Count / 1e3, color = Package)) +
+  pkgs <- unique(packages$Package)
+  pkgs <- c("forecast", pkgs[pkgs != "forecast"])
+  cols <- c(
+    "#000000",
+    '#e41a1c',
+    '#377ef8',
+    '#4daf4a',
+    '#984ea3',
+    '#a65628',
+    '#ff7f00',
+    '#999999',
+    '#f781bf',
+    "#80b1d3"
+  )
+  cols <- cols[seq_along(pkgs)]
+  names(cols) <- pkgs
+  downloads <- downloads |>
+    filter(Package %in% packages$Package, Month >= first_month) |>
+    mutate(Package = factor(Package, levels = pkgs))
+  if (nrow(packages) > 1) {
+    p <- downloads |> ggplot(aes(x = Month, y = Count / 1e3, color = Package))
+  } else {
+    p <- downloads |> ggplot(aes(x = Month, y = Count / 1e3))
+  }
+  p <- p +
     geom_line() +
-    labs(y = "Downloads (thousands)")
+    labs(y = "Downloads (thousands)") +
+    theme(
+      axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0))
+    )
+  if (!("forecast" %in% packages$Package)) {
+    p <- p + scale_y_continuous(breaks = seq(0, 100, 20), minor_breaks = NULL)
+  }
+  p +
+    scale_x_yearmonth(breaks = seq(first_month, length = 50, by = 24)) +
+    scale_color_manual(values = cols)
 }
--- _targets.R
+++ _targets.R
@@ -15,11 +15,25 @@ list(
   tar_target(top_ten_table, make_top_ten_table(top10)),
   tar_target(table1, make_table_history(head(history, 10))),
   tar_target(table2, make_table_history(tail(history, -10))),
-  tar_target(plot1, make_plot1(downloads)),
-  tar_target(plot2, make_plot2(downloads, top10)),
   tar_target(
-    plot3,
-    make_plot2(downloads, top10, first_month = yearmonth("2020-Jan"))
+    plot1,
+    make_download_plot(downloads, top10 |> filter(Package == "forecast"))
+  ),
+  tar_target(
+    plot2a,
+    make_download_plot(downloads, top10)
+  ),
+  tar_target(
+    plot2b,
+    make_download_plot(downloads, top10 |> filter(Package != "forecast"))
+  ),
+  tar_target(
+    plot2c,
+    make_download_plot(
+      downloads,
+      top10 |> filter(Package != "forecast"),
+      first_month = yearmonth("2020-Jan")
+    )
   ),
   tar_target(ets_benchmark, create_ets_benchmark()),
   tar_target(arima_benchmark, create_arima_benchmark()),
